faa83c616075afa2dfb681f4728cbb9832d47d8a
Ranveer Anand <ranveeranand85@gmail.com>
2022-03-17 15:33:53 +0530

    Star pattern Assignment

---
+++ src/com/kaizen/corejava/assignment/StarPattern11.java
@@ -0,0 +1,29 @@
+package com.kaizen.corejava.assignment;
+
+public class StarPattern11 {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 1 ; i <= n ; i++) {
+			
+			for(int j = 1 ; j <= i ; j ++) {
+				System.out.print("  ");
+			}
+			for(int j = i ; j <= n ; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+			
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
