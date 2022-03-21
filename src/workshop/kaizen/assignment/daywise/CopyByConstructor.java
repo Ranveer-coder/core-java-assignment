b77de33e3150d11f35e51caa8f2dfdf4c9b1499c
Ranveer-coder <66120572+Ranveer-coder@users.noreply.github.com>
2022-03-21 12:38:21 +0530

    Add files via upload
     Constructors

---
+++ src/workshop/kaizen/assignment/daywise/CopyByConstructor.java
@@ -0,0 +1,38 @@
+package workshop.kaizen.assignment.daywise;
+
+public class CopyByConstructor {
+	
+	int id;
+	String name;
+	
+	public CopyByConstructor( String name,int id){
+		this.id = id;
+		this.name = name;
+	}
+	//Copy constructor by constructor.
+	
+	CopyByConstructor(CopyByConstructor c){
+		id = c.id;
+		name = c.name;
+	}
+	
+
+	void describeCopy() {
+		System.out.println(name + " " + id);
+	}
+
+	public static void main(String[] args) {
+		CopyByConstructor c1 = new CopyByConstructor("Ranveer",7); 
+		CopyByConstructor c2 = new CopyByConstructor("Ranveer",7); 
+		CopyByConstructor c3 = new CopyByConstructor("Ranveer",7); 
+		CopyByConstructor c4 = new CopyByConstructor("Ranveer",7); 
+		CopyByConstructor c5 = new CopyByConstructor("Ranveer",7); 
+		
+		c1.describeCopy();
+		c2.describeCopy();
+		c3.describeCopy();
+		c4.describeCopy();
+		c5.describeCopy();
+	}
+
+}
