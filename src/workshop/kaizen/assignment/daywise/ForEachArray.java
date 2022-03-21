c47c3ca9c90b9bfef1d907a6575eacf6ccfbcfd1
Ranveer Anand <ranveeranand85@gmail.com>
2022-03-21 14:03:09 +0530

    new commit

---
+++ src/workshop/kaizen/assignment/daywise/ForEachArray.java
@@ -0,0 +1,15 @@
+package workshop.kaizen.assignment.daywise;
+
+public class ForEachArray {
+
+	public static void main(String[] args) {
+		
+		int[] a = {11,22,33,44,55};
+		
+		for(int i : a) {
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
